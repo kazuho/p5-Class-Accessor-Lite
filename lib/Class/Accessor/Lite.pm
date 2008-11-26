f981679b25b2d2fb0f401488c26dd27ce361c108
kazuho <kazuho@d0d07461-0603-4401-acd4-de1884942a52>
2008-11-26 08:06:42 +0000

    initial impl.
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Class-Accessor-Lite/trunk@24937 d0d07461-0603-4401-acd4-de1884942a52

---
+++ lib/Class/Accessor/Lite.pm
@@ -0,0 +1,25 @@
+package Class::Accessor::Lite;
+
+use strict;
+
+our $VERSION = '0.01';
+
+sub mk_accessors {
+    shift; # usage: Class::Accessor::Lite->mk_accessors(qw(...));
+    no strict 'refs';
+    my $pkg = caller(0);
+    for my $n (@_) {
+        *{$pkg . '::' . $n} = __m($n);
+    }
+}
+
+sub __m {
+    my $n = shift;
+    sub {
+        return $_[0]->{$n} if @_ == 1;
+        return $_[0]->{$n} = $_[1] if @_ == 2;
+        shift->{$n} = \@_;
+    };
+}
+
+1;
